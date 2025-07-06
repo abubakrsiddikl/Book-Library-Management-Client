aa39b23789b7cabb52896db6fa205db7560ac5f3
Abu Bakr Siddik <limonsk026@gmail.com>
2025-07-06 23:32:39 +0600

    setup redux and write farist RTK qurey getAll Books

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const baseApi = createApi({
+    reducerPath: "baseApi",
+    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000/api"}),
+    endpoints: (build)=>({
+        getAllBooks: build.query({
+            query: ()=> "/books"
+        })
+    })
+});
+
+export const {useGetAllBooksQuery} = baseApi;
